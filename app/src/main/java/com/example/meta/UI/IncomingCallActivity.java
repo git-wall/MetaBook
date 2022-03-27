f8d7d499e2d31905986170867d6a6a795d24983b
thanh_97 <thanhbp696@gmail.com>
2022-03-27 14:05:27 +0700

    Commit layout

---
+++ app/src/main/java/com/example/meta/UI/IncomingCallActivity.java
@@ -0,0 +1,16 @@
+package com.example.meta.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.meta.R;
+
+public class IncomingCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_call);
+    }
+}
---
+++ app/src/main/java/com/example/meta/UI/VideoCallActivity.java
@@ -0,0 +1,16 @@
+package com.example.meta.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.meta.R;
+
+public class VideoCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_call);
+    }
+}
